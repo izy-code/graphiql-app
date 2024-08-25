32a9049d7193eb5d9767630c6a2aae38f8a2fa83
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-25 18:38:53 +0300

    feat: add sign up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from 'next/navigation';
+import { type ReactNode } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
+import { RoutePath } from '@/common/enums';
+import { registrationSchema, type RegistrationSchemaType } from '@/common/validation-schema';
+import { CustomButton } from '@/components/custom-button/CustomButton';
+import { FormInputField } from '@/components/form-input-field/FormInputField';
+import { FormPasswordField } from '@/components/form-password-field/FormPasswordField';
+import { signInWithGoogle, signUp } from '@/firebase/firebase';
+
+import styles from './page.module.scss';
+
+export default function SignUp(): ReactNode {
+  const router = useRouter();
+
+  const {
+    formState: { errors, isDirty, isSubmitting, isValid },
+    handleSubmit,
+    register,
+  } = useForm<RegistrationSchemaType>({
+    mode: 'onChange',
+    resolver: yupResolver(registrationSchema),
+  });
+
+  const onValid: SubmitHandler<RegistrationSchemaType> = async (data) => {
+    if (!isValid) {
+      return;
+    }
+
+    const isSuccess = await signUp(data.name, data.email, data.password);
+
+    if (isSuccess) {
+      toast.success(`Successfully registered ${data.name}!`);
+      router.push(RoutePath.MAIN);
+    }
+  };
+
+  return (
+    <div className={styles.page}>
+      <h1>Registration page</h1>
+      <form className={styles.form} name="react-hook-form" noValidate onSubmit={handleSubmit(onValid)}>
+        <FormInputField label="Name" inputProps={{ ...register('name') }} error={errors.name?.message} />
+        <FormInputField
+          label="E-mail"
+          inputProps={{ ...register('email'), type: 'email' }}
+          error={errors.email?.message}
+        />
+
+        <fieldset className={styles.fieldset}>
+          <legend className={styles.legend}>Password</legend>
+          <FormPasswordField
+            label="Enter password"
+            inputProps={{ ...register('password') }}
+            error={errors.password?.message}
+          />
+          <FormInputField
+            label="Confirm password"
+            inputProps={{ ...register('passwordConfirm'), type: 'password', autoComplete: 'new-password' }}
+            error={errors.passwordConfirm?.message}
+          />
+        </fieldset>
+
+        <CustomButton className={styles.submitButton} type="submit" disabled={isSubmitting || !isDirty || !isValid}>
+          Sign up
+        </CustomButton>
+        <CustomButton
+          className={styles.submitButton}
+          variant="secondary"
+          onClick={async () => {
+            const userName = await signInWithGoogle();
+
+            if (userName) {
+              toast.success(`Successfully signed ${userName} with Google account!`);
+              router.push(RoutePath.MAIN);
+            }
+          }}
+        >
+          Sign up with Google
+        </CustomButton>
+      </form>
+    </div>
+  );
+}
