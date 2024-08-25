0b48b1af655fa8245810ce3818e71f20e53e2785
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-25 18:37:19 +0300

    feat: update root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import { type ReactNode } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from '@/firebase/firebase';
 
 import styles from './page.module.scss';
 
 export default function Home(): ReactNode {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className={styles.page}>
-      <h1>REST/GraphiQL client</h1>
+      <h1>Main page</h1>
+      {loading ? (
+        <p>Loading firebase...</p>
+      ) : (
+        <p>{user ? `Welcome back, ${user?.displayName}!` : 'Welcome, please sign in.'}</p>
+      )}
     </div>
   );
 }
