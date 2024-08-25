bc662c0c1381101dbe5924926da501d073adad38
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-25 18:31:57 +0300

    feat: add Yup validation schema

---
+++ src/common/constants.ts
@@ -0,0 +1,6 @@
+export const MIN_PASSWORD_LENGTH = 8;
+export const EMAIL_REGEX = /^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i;
+export const LOWERCASE_LETTER_REGEX = /\p{Ll}/u;
+export const UPPERCASE_LETTER_REGEX = /\p{Lu}/u;
+export const SPECIAL_CHARACTER_REGEX = /[-+:|/\\%*#@$!?^&]/;
+export const NUMBER_REGEX = /\d/;
