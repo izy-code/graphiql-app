e5989e8e0a3274904a33d5164506f966e026eccb
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-26 16:38:53 +0300

    fix: del loader and add useAuth hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 'use client';
 
 import { type ReactNode } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 
-import { auth } from '@/firebase/firebase';
+import { useAuth } from '@/hooks/useAuth';
 
 import styles from './page.module.scss';
 
 export default function Home(): ReactNode {
-  const [user, loading] = useAuthState(auth);
+  const { user } = useAuth();
 
   return (
     <div className={styles.page}>
       <h1>Main page</h1>
-      {loading ? (
-        <p>Loading firebase...</p>
-      ) : (
-        <p>{user ? `Welcome back, ${user?.displayName}!` : 'Welcome, please sign in.'}</p>
-      )}
+      <p>{user ? `Welcome back, ${user?.displayName}!` : 'Welcome, please sign in.'}</p>
     </div>
   );
 }
