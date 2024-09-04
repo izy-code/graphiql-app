76943c2edc53b8c4774154ba4f4c00448b0b3989
Izy <126877709+izy-code@users.noreply.github.com>
2024-09-04 18:55:56 +0300

    feat: add url change an fetch to graphql client

--- src/page-components/graphql/Graphql.tsx
+++ src/page-components/graphql/Graphql.tsx
@@ -1,38 +1,154 @@
 'use client';
 
 import Box from '@mui/material/Box';
-import { type ReactNode, useState } from 'react';
-import * as React from 'react';
+import clsx from 'clsx';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 
-import { makeRequest, makeRequest1 } from '@/common/graphQlApi.ts';
+import { ProtectedPaths } from '@/common/enums.ts';
+import { makeRequest } from '@/common/graphQlApi.ts';
 import { AuthRoute } from '@/components/auth-route/AuthRoute';
+import type { IData } from '@/components/client-table/types.ts';
 import { CustomButton } from '@/components/custom-button/CustomButton.tsx';
+import { useCurrentLocale } from '@/locales/client.ts';
+import { decodeBase64, encodeBase64, generateUniqueId } from '@/utils/utils.ts';
 
+import ClientTable from '../../components/client-table/ClientTable.tsx';
 import CustomInput from '../../components/custom-input/CustomInput.tsx';
-import CustomTextarea from '../../components/custom-textarea/CustomTextarea.tsx';
-import TableHeaders from '../../components/table-headers/TableHeaders.tsx';
+import CustomTextarea from '../../components/custom-textarea/CustomTextarea';
 import styles from './Graphql.module.scss';
 
+interface RequestBody {
+  query: string;
+  variables: string;
+}
+
 function GraphQl(): ReactNode {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const locale = useCurrentLocale();
+
   const [query, setQuery] = useState('');
-  const [url, setUrl] = useState('');
   const [variables, setVariables] = useState('');
-  const [status] = useState(200);
-  const [responseBody] = useState('{}');
+  const [status, setStatus] = useState('');
+  const [responseBody, setResponseBody] = useState('');
+  const [endpoint, setEndpoint] = useState('');
+  const [sdl, setSdl] = useState('');
+  const [headers, setHeaders] = useState<IData[]>([]);
+  const didMount = useRef(false);
+  console.log(didMount.current);
+  console.log(pathname);
+  console.log('headers', headers);
 
-  const onRequest = async () => {
-    if (query) {
-      const { data, error } = await makeRequest(url, { query });
+  useEffect(() => {
+    if (!didMount.current) {
+      console.log('did mount');
+      const pathParts: string[] = pathname.split('/').filter((_, index) => index > 2);
+      const [endpointPart, requestBodyPart] = pathParts;
 
-      if (error && error instanceof Error) {
-        toast.error(error.message);
+      if (endpointPart) {
+        const decodedEndpoint = decodeBase64(endpointPart);
+        setEndpoint(decodedEndpoint);
+        setSdl(`${decodedEndpoint}?sdl`);
       }
-      if (data) {
-        const code = JSON.stringify(data, null, 2);
-        console.log(data, code);
+
+      const decodedHeaders: IData[] = [];
+      searchParams.forEach((value, key) => {
+        decodedHeaders.push({ id: generateUniqueId(), key, value: decodeURIComponent(value) });
+      });
+      setHeaders(decodedHeaders);
+
+      if (requestBodyPart) {
+        try {
+          const parsedRequestBody = JSON.parse(decodeBase64(requestBodyPart || '{}')) as RequestBody;
+          setQuery(parsedRequestBody.query);
+          setVariables(parsedRequestBody.variables);
+        } catch (error) {
+          toast.error(`Can't parse request body in URL`);
+        }
       }
+
+      didMount.current = true;
     }
+  }, [pathname, searchParams]);
+
+  const urlStart = `/${locale}${ProtectedPaths.GRAPHQL}/`;
+
+  const getEncodedRequestBody = (): string => {
+    const requestBody = JSON.stringify({
+      query,
+      variables,
+    });
+    return encodeBase64(requestBody);
+  };
+
+  const getEncodedHeaders = (headersParameter: IData[] = headers): string => {
+    if (!headersParameter.length) {
+      return '';
+    }
+
+    const encodedHeaders: string[] = [];
+    headersParameter.forEach((header) => {
+      encodedHeaders.push(`${header.key}=${encodeURIComponent(header.value)}`);
+    });
+
+    return `?${encodedHeaders.join('&')}`;
+  };
+
+  const handleRequest = async (): Promise<void> => {
+    setStatus('');
+    setResponseBody('');
+
+    const { status: statusCode, data, errorMessage } = await makeRequest(endpoint, query, variables, headers);
+
+    if (errorMessage) {
+      toast.error(errorMessage);
+      return;
+    }
+
+    setStatus(statusCode!.toString());
+    setResponseBody(JSON.stringify(data, null, 2));
+  };
+
+  const handleEndpointChange = (evt: React.ChangeEvent<HTMLInputElement>): void => {
+    setEndpoint(evt.target.value);
+
+    if (evt.target.value) {
+      setSdl(`${evt.target.value}?sdl`);
+    } else {
+      setSdl('');
+    }
+
+    if (query || variables) {
+      window.history.replaceState(
+        null,
+        '',
+        `${urlStart}${encodeBase64(evt.target.value)}/${getEncodedRequestBody()}${getEncodedHeaders()}`,
+      );
+      return;
+    }
+
+    window.history.replaceState(null, '', `${urlStart}${encodeBase64(evt.target.value)}${getEncodedHeaders()}`);
+  };
+
+  const handleJsonEditorBlur = (): void => {
+    if (!query && !variables) {
+      window.history.replaceState(null, '', `${urlStart}${encodeBase64(endpoint)}${getEncodedHeaders()}`);
+      return;
+    }
+
+    window.history.replaceState(
+      null,
+      '',
+      `${urlStart}${encodeBase64(endpoint)}/${getEncodedRequestBody()}${getEncodedHeaders()}`,
+    );
+  };
+
+  const handleHeadersChange = (changedHeaders: IData[]): void => {
+    setHeaders(changedHeaders);
+
+    window.history.replaceState(null, '', `${pathname}${getEncodedHeaders(changedHeaders)}`);
   };
 
   return (
@@ -41,29 +157,50 @@ function GraphQl(): ReactNode {
         <h1 className={styles.title}>GraphiQl Client</h1>
         <div className={styles.items}>
           <h2 className={styles.sectionTitle}>URL</h2>
-          <CustomInput label="Endpoint URL" variant="standard" width="420px" onChange={(e) => setUrl(e.target.value)} />
-          <CustomInput label="SDL URL" variant="standard" width="420px" />
-          <CustomButton onClick={onRequest}>Request</CustomButton>
+          <CustomInput
+            label="Endpoint URL"
+            variant="outlined"
+            value={endpoint}
+            width="100%"
+            onChange={handleEndpointChange}
+          />
+          <CustomInput
+            label="SDL URL"
+            variant="outlined"
+            value={sdl}
+            width="100%"
+            onChange={(e) => setSdl(e.target.value)}
+          />
+          <CustomButton className={styles.requestButton} onClick={handleRequest}>
+            Request
+          </CustomButton>
         </div>
         <div className={styles.section}>
-          <h2 className={styles.sectionTitle}>Params</h2>
-          <TableHeaders />
           <div className={styles.item}>
             <h4>Query: </h4>
-            <CustomTextarea label="Query" value={query} width="500px" onChange={(e) => setQuery(e.target.value)} />
+            <CustomTextarea
+              label="Query"
+              value={query}
+              width="100%"
+              onBlur={handleJsonEditorBlur}
+              onChange={(e) => setQuery(e.target.value)}
+            />
           </div>
+          <h2 className={styles.sectionTitle}>Params</h2>
+          <ClientTable title="Headers" tableInfo={headers} onChange={handleHeadersChange} />
           <div className={styles.item}>
             <h4>Variables: </h4>
             <CustomTextarea
               label="Variables"
               value={variables}
-              width="500px"
+              width="100%"
+              onBlur={handleJsonEditorBlur}
               onChange={(e) => setVariables(e.target.value)}
             />
           </div>
         </div>
       </div>
-      <div className={styles.section}>
+      <div className={clsx(styles.section, styles.response)}>
         <h2 className={styles.sectionTitle}>Response</h2>
         <div className={styles.oneLine}>
           <h4>Status:</h4>
@@ -71,7 +208,10 @@ function GraphQl(): ReactNode {
         </div>
         <div className={styles.body}>
           <h4>Body:</h4>
-          <Box component="pre" sx={{ backgroundColor: 'inherit', p: 2, mt: 1, maxHeight: '400px', overflow: 'auto' }}>
+          <Box
+            component="pre"
+            sx={{ backgroundColor: 'inherit', p: 2, mt: 1, maxHeight: '400px', overflow: 'auto', width: '100%' }}
+          >
             {responseBody}
           </Box>
         </div>
