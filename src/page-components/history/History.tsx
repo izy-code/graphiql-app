286012a249a9851d879e1e84894ed0d3c0409d37
Izy <126877709+izy-code@users.noreply.github.com>
2024-09-04 18:50:12 +0300

    fix: add teammate history changes

--- src/page-components/history/History.tsx
+++ src/page-components/history/History.tsx
@@ -1,19 +1,27 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import type { ReactNode } from 'react';
 import * as React from 'react';
 
+import { LocalStorageKeys, ProtectedPaths } from '@/common/enums';
 import { AuthRoute } from '@/components/auth-route/AuthRoute';
+import { decodeBase64 } from '@/utils/utils';
 
-import { CustomMuiButton } from '../../components/custom-mui-button/CustomMuiButton.tsx';
 import styles from './History.module.scss';
 
 function History(): ReactNode {
-  const example = ['http://localhost:3000/GET', 'http://localhost:3000/POST', 'http://localhost:3000/DELETE'];
-  const router = useRouter();
-  const [requests] = React.useState<string[]>(example);
+  const example = JSON.parse(localStorage.getItem(LocalStorageKeys.URLS_RSS_REQUEST) || '[]') as string[];
 
+  const [requests, setRequests] = React.useState<string[]>(example);
+
+  React.useEffect(() => {
+    const storedRequests = localStorage.getItem(LocalStorageKeys.URLS_RSS_REQUEST);
+    if (storedRequests) {
+      const parsedRequests = JSON.parse(storedRequests) as string[];
+      setRequests(parsedRequests);
+    }
+  }, []);
   return (
     <div className={styles.historyPage}>
       <h1 className={styles.historyTitle}>History</h1>
@@ -22,8 +30,8 @@ function History(): ReactNode {
           <h2>You haven&apos;t executed any requests</h2>
           <p>It&apos;s empty here. Try: </p>
           <div className={styles.buttonGroup}>
-            <CustomMuiButton onClick={() => router.push('/rest')}>REST Client</CustomMuiButton>
-            <CustomMuiButton onClick={() => router.push('/graph')}>GraphiQL Client</CustomMuiButton>
+            <Link href={ProtectedPaths.REST}>REST Client</Link>
+            <Link href={ProtectedPaths.GRAPHQL}>GraphiQL Client</Link>
           </div>
         </div>
       ) : (
@@ -31,11 +39,21 @@ function History(): ReactNode {
           <div className={styles.section}>
             <h2 className={styles.sectionTitle}> Requests:</h2>
             <div className={styles.requestsStrings}>
-              {requests.map((request) => (
-                <div key={request} className={styles.requestItem}>
-                  {request}
-                </div>
-              ))}
+              {requests.map((request) => {
+                const pathParts = request.split('/');
+                const methodParam = pathParts[2] || '';
+                const encodedEndpoint = pathParts[3] || '';
+                const decodedEndpoint = decodeBase64(encodedEndpoint);
+
+                return (
+                  <div key={request} className={styles.requestItem}>
+                    <Link className={styles.oneLine} href={request}>
+                      <div>{`[${methodParam}`}</div>
+                      <div>{`${decodedEndpoint}]`}</div>
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
