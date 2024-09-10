d56212f810b3dd21dde42e3e83fe4c9897780ff5
Izy <126877709+izy-code@users.noreply.github.com>
2024-09-11 00:17:39 +0300

    fix: change toastify styles file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './global.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import clsx from 'clsx';
